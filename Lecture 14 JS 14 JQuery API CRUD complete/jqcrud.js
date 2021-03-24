4b69f998fed6c8ce90547acb238b77a503787417
Hassaan Asif Khan <hak12b@gmail.com>
2021-03-24 19:22:00 +0500

    1st

---
+++ Lecture 14 JS 14 JQuery API CRUD complete/jqcrud.js
@@ -0,0 +1,23 @@
+$(function () {
+  $("#recipies-main").click(read_recipies);
+});
+function read_recipies() {
+  $ajax({
+    url: "https://usman-recipes.herokuapp.com/api/recipes",
+    method: "GET",
+    success: function (response) {
+      console.log(response);
+      var recipes = $("#recipies-main");
+      recipes.empty();
+      for (var i = 0; i < response.length; i++) {
+        var temp = response[i];
+        recipes.append(
+          <div class="recipie">
+            <h3>${temp.title}</h3>
+            <p>${temp.body}</p>
+          </div>
+        );
+      }
+    },
+  });
+}
