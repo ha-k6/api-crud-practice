08988defe9d4b5cdd73602799fad3c667a059677
Hassaan Asif Khan <hak12b@gmail.com>
2021-03-25 02:50:38 +0500

    Update jqcrud.js

--- Lecture 14 JS 14 JQuery API CRUD complete/jqcrud.js
+++ Lecture 14 JS 14 JQuery API CRUD complete/jqcrud.js
@@ -12,7 +12,7 @@ function read_recipies() {
       for (var i = 0; i < response.length; i++) {
         var temp = response[i];
         recipes.append(
-          `<div class="recipie"><h3>${temp.title}</h3><p>${temp.body}</p></div>`
+          `<div class="recipie"><h3>${temp.title}</h3> <button class="btn btn-warning btn-sm float-right">Update</button><button class="btn  btn-danger btn-sm float-right">Delete</button> <p>${temp.body}</p></div>`
         );
       }
     },
